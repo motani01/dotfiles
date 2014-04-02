94b38a4938ac011eeeffea7fce88308456c49f59
motani <motani@yahoo-corp.jp>
2014-04-02 16:53:39 +0900

    add

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,5 +6,5 @@ ln -s ~/dotfiles/tmux.conf	~/.tmux.conf
 ln -s ~/dotfiles/vimrc		~/.vimrc
 ln -s ~/dotfiles/zshrc	 	~/.zshrc
 ln -s ~/dotfiles/vim	 	~/.vim
-ln -s ~/dotfiles/oh-my-zsh	~/.oh-my-zsh
+ln -s ~/dotfiles/zsh	~/.zsh
 
